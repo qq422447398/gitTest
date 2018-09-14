c156584839478af97e30196b115a863bf2027be1
zhangweijie <422447398@qq.com>
2018-09-14 14:54:46 +0800

    first commit

---
+++ src/ClassA.java
@@ -0,0 +1,15 @@
+public class ClassA {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ClassA(int id) {
+        this.id = id;
+    }
+}
---
+++ src/ClassB.java
@@ -0,0 +1,15 @@
+public class ClassB {
+    private String line;
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public ClassB(String line) {
+        this.line = line;
+    }
+}
